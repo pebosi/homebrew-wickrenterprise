e49c536f2fc1adb1e7145a5b6dbf64d3936c3384
pebosi <mail@pebosi.net>
2021-10-12 15:11:31 +0200

    update to 5.86.13

--- Casks/wickrenterprise.rb
+++ Casks/wickrenterprise.rb
@@ -1,6 +1,6 @@
 cask 'wickrenterprise' do
-  version '5.86.10'
-  sha256 'd58042ae36886ef39343a9b7c5f5c030dda8cf562162e7b5ec322b0e7adc1e90'
+  version '5.86.13'
+  sha256 '1e14ee554d77014a3ef4ffc685936e559ee06e9c7f2eb1d0207704af26f2f1ac'
 
   # amazonaws.com/static.wickr.com/downloads/mac/ent was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/ent/WickrEnterprise-#{version}.dmg"
